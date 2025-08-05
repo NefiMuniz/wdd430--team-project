1e72c8a80ef90bdacedaad06096813b88b44c3de
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2025-08-04 23:41:42 -0300

    fixed route.ts

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,26 +2,28 @@ import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import jwt from 'jsonwebtoken';
+import postgres from "postgres";
 
-const mockUser = {
-    email: 'mateus@example.com',
-    passwordHash: bcrypt.hashSync('123456', 10),
-};
+const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
     const { email, password } = body;
 
-    if (email !== mockUser.email) {
+    const users = await sql`SELECT * FROM users WHERE email = ${email}`;
+
+    if (users.length === 0) {
         return NextResponse.json({ message: 'User not found' }, { status: 401 });
     }
 
-    const isPasswordValid = bcrypt.compareSync(password, mockUser.passwordHash);
+    const user = users[0];
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
     if (!isPasswordValid) {
         return NextResponse.json({ message: 'Invalid password' }, { status: 401 });
     }
 
-    const token = jwt.sign({ email }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET!, { expiresIn: '1h' });
 
     const response = NextResponse.json({ message: 'login successful' });
 
